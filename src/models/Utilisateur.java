979257f28525425c29f7c4b9ac5a08153b4d50ff
papaweurseuckdieng <kirikoupapacha@gmail.com>
2025-06-15 15:26:08 +0000

    first commit

---
+++ src/models/Utilisateur.java
@@ -0,0 +1,105 @@
+package models;
+
+public class Utilisateur {
+    private int idUser;
+    private String nom;
+    private String prenom;
+    private String telephone;
+    private String adresse;
+    private String role;
+    private String genre;
+    private String email;
+    private String mdp;
+
+    public Utilisateur() {}
+
+    public Utilisateur(int idUser, String nom, String prenom, String telephone, String adresse,
+                       String role, String genre, String email, String mdp) {
+        this.idUser = idUser;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+        this.adresse = adresse;
+        this.role = role;
+        this.genre = genre;
+        this.email = email;
+        this.mdp = mdp;
+    }
+
+    // Getters et Setters
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getPassword() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
